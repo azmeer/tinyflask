d89872b5b4cc48c605b6d187f878dca59620717b
azmeer <azmeer.salleh@gmail.com>
2017-11-01 18:09:29 -0700

    adds app settings from environment variables in __init__

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,12 @@
 # project/__init__.py
 
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # set config
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
 
 @app.route('/ping', methods=['GET'])
