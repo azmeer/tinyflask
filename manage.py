dd24307fd240e7570339f1f79b5545afc10d8aa2
azmeer <azmeer.salleh@gmail.com>
2017-11-03 00:22:40 -0700

    adds basic test cases and adds flask-testing to requirements

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ from flask_script import Manager
 
 from project import app, db
 
+import unittest
+
 manager = Manager(app)
 
 @manager.command
@@ -13,6 +15,12 @@ def recreate_db():
     db.create_all()
     db.session.commit()
 
+@manager.command
+def test():
+    """Runs tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    return 0 if result.wasSuccessful() else 1
 
 if __name__ == '__main__':
     manager.run()
